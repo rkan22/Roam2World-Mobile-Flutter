e7772ea2d5e9d9849d56f6a86371ad0f2fb77538
rkan22 <110312528+rkan22@users.noreply.github.com>
2026-08-06 00:00:28 +0300

    Refine mobile color system

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primary = Color(0xFF0B5FFF);
-  static const primaryDark = Color(0xFF0846C8);
-  static const primaryLight = Color(0xFFEAF2FF);
+  static const primary = Color(0xFF2563EB);
+  static const primaryDark = Color(0xFF1D4ED8);
+  static const primaryLight = Color(0xFFEFF6FF);
+  static const primarySoft = Color(0xFFDBEAFE);
 
-  static const navy = Color(0xFF071B3A);
-  static const background = Color(0xFFF6F9FF);
+  static const navy = Color(0xFF0F172A);
+  static const background = Color(0xFFF8FAFC);
   static const card = Color(0xFFFFFFFF);
+  static const surfaceMuted = Color(0xFFF1F5F9);
 
-  static const textPrimary = Color(0xFF101828);
-  static const textSecondary = Color(0xFF667085);
-  static const border = Color(0xFFE4E7EC);
+  static const textPrimary = Color(0xFF0F172A);
+  static const textSecondary = Color(0xFF64748B);
+  static const textMuted = Color(0xFF94A3B8);
+  static const border = Color(0xFFE2E8F0);
 
-  static const success = Color(0xFF12B76A);
-  static const warning = Color(0xFFF79009);
-  static const danger = Color(0xFFF04438);
+  static const success = Color(0xFF16A34A);
+  static const successSoft = Color(0xFFDCFCE7);
+  static const warning = Color(0xFFF59E0B);
+  static const warningSoft = Color(0xFFFEF3C7);
+  static const danger = Color(0xFFDC2626);
+  static const dangerSoft = Color(0xFFFEE2E2);
 
   static const orange = Color(0xFFFF7900);
   static const vodafone = Color(0xFFE60000);
