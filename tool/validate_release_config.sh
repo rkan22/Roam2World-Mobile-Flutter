d8365b0ad437514b0e739686ef8e956f28f8b982
rkan22 <110312528+rkan22@users.noreply.github.com>
2026-08-07 02:18:24 +0300

    test: validate release icon assets

--- tool/validate_release_config.sh
+++ tool/validate_release_config.sh
@@ -21,8 +21,14 @@ require_file android/app/build.gradle.kts
 require_file android/app/src/main/AndroidManifest.xml
 require_file ios/Flutter/Release.xcconfig
 require_file ios/Runner/Info.plist
+require_file ios/Runner/Assets.xcassets/AppIcon.appiconset/Contents.json
 require_file lib/core/config/app_environment.dart
 require_file .gitignore
+
+for density in mdpi hdpi xhdpi xxhdpi xxxhdpi; do
+  require_file "android/app/src/main/res/mipmap-${density}/ic_launcher.png"
+done
+require_file ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-1024x1024@1x.png
 
 require_match '^version: [0-9]+\.[0-9]+\.[0-9]+\+[0-9]+$' pubspec.yaml
 require_match 'applicationId = "com\.roam2world\.b2b"' android/app/build.gradle.kts
@@ -34,6 +40,7 @@ require_match 'android.permission.INTERNET' android/app/src/main/AndroidManifest
 require_match 'android:label="Roam2World B2B"' android/app/src/main/AndroidManifest.xml
 require_match 'PRODUCT_BUNDLE_IDENTIFIER=com\.roam2world\.b2b' ios/Flutter/Release.xcconfig
 require_match '<string>Roam2World B2B</string>' ios/Runner/Info.plist
+require_match 'Icon-App-1024x1024@1x\.png' ios/Runner/Assets.xcassets/AppIcon.appiconset/Contents.json
 require_match "static const String appName = 'Roam2World B2B';" lib/core/config/app_environment.dart
 require_match '^/android/key\.properties$' .gitignore
 require_match '^\*\.jks$' .gitignore
