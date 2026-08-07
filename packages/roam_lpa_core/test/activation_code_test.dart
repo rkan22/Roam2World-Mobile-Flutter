9d4ac8a84a64c505e53922df969aec9a7bb7102d
rkan22 <110312528+rkan22@users.noreply.github.com>
2026-08-07 18:12:45 +0300

    Fix activation parser test for root CI

--- packages/roam_lpa_core/test/activation_code_test.dart
+++ packages/roam_lpa_core/test/activation_code_test.dart
@@ -1,21 +1,24 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:roam_lpa_core/roam_lpa_core.dart';
-import 'package:test/test.dart';
 
 void main() {
   test('parses canonical activation code', () {
-    final code = LpaActivationCode.parse('LPA:1$smdp.example$MATCH123');
+    final code = LpaActivationCode.parse(r'LPA:1$smdp.example$MATCH123');
     expect(code.smdpAddress, 'smdp.example');
     expect(code.matchingId, 'MATCH123');
-    expect(code.canonical, 'LPA:1$smdp.example$MATCH123');
+    expect(code.canonical, r'LPA:1$smdp.example$MATCH123');
   });
 
   test('normalizes lowercase prefix and confirmation code', () {
-    final code = LpaActivationCode.parse('lpa:1$smdp.example$MATCH123$9876');
+    final code = LpaActivationCode.parse(r'lpa:1$smdp.example$MATCH123$9876');
     expect(code.confirmationCode, '9876');
-    expect(code.canonical, 'LPA:1$smdp.example$MATCH123$9876');
+    expect(code.canonical, r'LPA:1$smdp.example$MATCH123$9876');
   });
 
   test('rejects incomplete code', () {
-    expect(() => LpaActivationCode.parse('LPA:1$smdp.example'), throwsFormatException);
+    expect(
+      () => LpaActivationCode.parse(r'LPA:1$smdp.example'),
+      throwsFormatException,
+    );
   });
 }
