27ea3cf406cfd8300b72ba95c028fbd9d838f0bb
Erkan aydoğan <erkanaydogan@Erkan-iMac.local>
2026-08-13 17:16:14 +0300

    chore: remove stale reports placeholder file

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -6,10 +6,26 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <ccid/ccid_plugin.h>
+#include <desktop_drop/desktop_drop_plugin.h>
 #include <flutter_secure_storage_linux/flutter_secure_storage_linux_plugin.h>
+#include <gtk/gtk_plugin.h>
+#include <url_launcher_linux/url_launcher_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) ccid_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "CcidPlugin");
+  ccid_plugin_register_with_registrar(ccid_registrar);
+  g_autoptr(FlPluginRegistrar) desktop_drop_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "DesktopDropPlugin");
+  desktop_drop_plugin_register_with_registrar(desktop_drop_registrar);
   g_autoptr(FlPluginRegistrar) flutter_secure_storage_linux_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterSecureStorageLinuxPlugin");
   flutter_secure_storage_linux_plugin_register_with_registrar(flutter_secure_storage_linux_registrar);
+  g_autoptr(FlPluginRegistrar) gtk_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "GtkPlugin");
+  gtk_plugin_register_with_registrar(gtk_registrar);
+  g_autoptr(FlPluginRegistrar) url_launcher_linux_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "UrlLauncherPlugin");
+  url_launcher_plugin_register_with_registrar(url_launcher_linux_registrar);
 }
