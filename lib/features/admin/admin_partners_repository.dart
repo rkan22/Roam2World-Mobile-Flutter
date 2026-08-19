801d8219dcb0eb246d90effb9bb826cb06c82832
rkan22 <110312528+rkan22@users.noreply.github.com>
2026-08-19 21:15:20 +0300

    feat(admin): add reseller detail and account update API

--- lib/features/admin/admin_partners_repository.dart
+++ lib/features/admin/admin_partners_repository.dart
@@ -3,22 +3,13 @@ import '../../core/api/api_endpoints.dart';
 import 'admin_partners_data.dart';
 
 class AdminPartnersRepository {
-  AdminPartnersRepository({ApiClient? apiClient})
-    : _apiClient = apiClient ?? ApiClient();
-
+  AdminPartnersRepository({ApiClient? apiClient}) : _apiClient = apiClient ?? ApiClient();
   final ApiClient _apiClient;
 
-  Future<AdminPartnerList> fetchResellers() {
-    return _apiClient.get<AdminPartnerList>(
-      '/api/v1/resellers/resellers/',
-      parser: AdminPartnerList.fromResponse,
-    );
-  }
+  Future<AdminPartnerList> fetchResellers() => _apiClient.get<AdminPartnerList>('/api/v1/resellers/resellers/', parser: AdminPartnerList.fromResponse);
+  Future<AdminPartnerList> fetchDealers() => _apiClient.get<AdminPartnerList>(ApiEndpoints.mobileAdminDealers, parser: AdminPartnerList.fromResponse);
 
-  Future<AdminPartnerList> fetchDealers() {
-    return _apiClient.get<AdminPartnerList>(
-      ApiEndpoints.mobileAdminDealers,
-      parser: AdminPartnerList.fromResponse,
-    );
-  }
+  Future<AdminResellerDetail> fetchResellerDetail(int id) => _apiClient.get<AdminResellerDetail>('/api/v1/resellers/resellers/$id/', parser: AdminResellerDetail.fromResponse);
+
+  Future<AdminResellerDetail> updateReseller(int id, Map<String, dynamic> payload) => _apiClient.patch<AdminResellerDetail>('/api/v1/resellers/resellers/$id/', data: payload, parser: AdminResellerDetail.fromResponse);
 }
