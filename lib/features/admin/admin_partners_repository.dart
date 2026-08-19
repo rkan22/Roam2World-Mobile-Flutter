ce1c532819a24d2c7da08edb7e969d30060f1b0d
rkan22 <110312528+rkan22@users.noreply.github.com>
2026-08-19 22:24:04 +0300

    fix(admin): load dealers from real dealer serializer

--- lib/features/admin/admin_partners_repository.dart
+++ lib/features/admin/admin_partners_repository.dart
@@ -7,7 +7,7 @@ class AdminPartnersRepository {
   final ApiClient _apiClient;
 
   Future<AdminPartnerList> fetchResellers() => _apiClient.get<AdminPartnerList>('/api/v1/resellers/resellers/', parser: AdminPartnerList.fromResponse);
-  Future<AdminPartnerList> fetchDealers() => _apiClient.get<AdminPartnerList>(ApiEndpoints.mobileAdminDealers, parser: AdminPartnerList.fromResponse);
+  Future<AdminPartnerList> fetchDealers() => _apiClient.get<AdminPartnerList>('/api/v1/resellers/dealers/', parser: AdminPartnerList.fromResponse);
 
   Future<AdminResellerDetail> fetchResellerDetail(int id) => _apiClient.get<AdminResellerDetail>('/api/v1/resellers/resellers/$id/', parser: AdminResellerDetail.fromResponse);
   Future<AdminResellerDetail> updateReseller(int id, Map<String, dynamic> payload) => _apiClient.patch<AdminResellerDetail>('/api/v1/resellers/resellers/$id/', data: payload, parser: AdminResellerDetail.fromResponse);
