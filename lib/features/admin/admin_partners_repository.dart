22f2b530a691eb0039489373c8df83f180d722ae
rkan22 <110312528+rkan22@users.noreply.github.com>
2026-08-19 22:23:35 +0300

    feat(admin): connect dealer detail update and suspend

--- lib/features/admin/admin_partners_repository.dart
+++ lib/features/admin/admin_partners_repository.dart
@@ -10,6 +10,9 @@ class AdminPartnersRepository {
   Future<AdminPartnerList> fetchDealers() => _apiClient.get<AdminPartnerList>(ApiEndpoints.mobileAdminDealers, parser: AdminPartnerList.fromResponse);
 
   Future<AdminResellerDetail> fetchResellerDetail(int id) => _apiClient.get<AdminResellerDetail>('/api/v1/resellers/resellers/$id/', parser: AdminResellerDetail.fromResponse);
-
   Future<AdminResellerDetail> updateReseller(int id, Map<String, dynamic> payload) => _apiClient.patch<AdminResellerDetail>('/api/v1/resellers/resellers/$id/', data: payload, parser: AdminResellerDetail.fromResponse);
+
+  Future<AdminDealerDetail> fetchDealerDetail(int id) => _apiClient.get<AdminDealerDetail>('/api/v1/mobile/admin/dealers/$id/', parser: AdminDealerDetail.fromResponse);
+  Future<AdminDealerDetail> updateDealer(int id, Map<String, dynamic> payload) => _apiClient.patch<AdminDealerDetail>('/api/v1/mobile/admin/dealers/$id/', data: payload, parser: AdminDealerDetail.fromResponse);
+  Future<AdminDealerDetail> setDealerStatus(int id, {required bool suspend, String reason = ''}) => _apiClient.post<AdminDealerDetail>('/api/v1/mobile/admin/dealers/$id/status/', data: {'action': suspend ? 'suspend' : 'activate', 'reason': reason}, parser: AdminDealerDetail.fromResponse);
 }
